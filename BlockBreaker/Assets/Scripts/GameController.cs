f33885e2ee8e8b4f040a5108ecd8225a122bc850
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2024-07-29 22:24:19 -0300

    Adding a ball, first shoot and ball & paddle limits

--- BlockBreaker/Assets/Scripts/GameController.cs
+++ BlockBreaker/Assets/Scripts/GameController.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameStates
+{
+    Ready, Play
+}
 public class GameController : MonoBehaviour
 {
+    public static GameController instance;
+    public GameObject ball;
+    GameStates state;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
@@ -15,4 +22,20 @@ public class GameController : MonoBehaviour
     {
         
     }
+    public GameStates GetGameState()
+    {
+        return state;
+    }
+    public void SetGameState(GameStates newState)
+    {
+        state = newState;
+        switch (state)
+        {
+            case GameStates.Ready:
+                break;
+            case GameStates.Play:
+                break;
+        }
+    }
+
 }
--- BlockBreaker/Assets/Scripts/PlayerInputs.cs
+++ BlockBreaker/Assets/Scripts/PlayerInputs.cs
@@ -6,22 +6,33 @@ public class PlayerInputs : MonoBehaviour
 {
 
     [SerializeField] Transform paddle;
-
-    public float speed = 5;
+    [SerializeField] Transform limitLeft, limitRight;
+    Rigidbody2D ball;
+    public float paddleSpeed = 5;
+    public float ballSpeed = 5;
     Vector2 mousePos { get { return Camera.main.ScreenToWorldPoint(Input.mousePosition); } }
     // Start is called before the first frame update
     void Start()
     {
-
+        ball = GameController.instance.ball.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        paddle.position = new Vector2(Mathf.Clamp(mousePos.x, limitLeft.position.x, limitRight.position.x), paddle.position.y);
     }
 
     private void OnMouseOver()
     {
-        paddle.position = new Vector2(mousePos.x, paddle.position.y);
+        //paddle.position = new Vector2(mousePos.x, paddle.position.y);
+    }
+    private void OnMouseDown()
+    {
+        if (GameController.instance.GetGameState().Equals(GameStates.Ready))
+        {
+            ball.transform.SetParent(null);
+            ball.velocity = new Vector2(ballSpeed, ballSpeed);
+        }
     }
 }
